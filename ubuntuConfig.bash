8f5368854137f4a8be408034ab6227297bf846c9
Jason Gmitter <jgmitter@cisco.com>
2019-06-07 11:38:40 -0400

    Update ubuntuConfig.bash

--- ubuntuConfig.bash
+++ ubuntuConfig.bash
@@ -2,6 +2,7 @@
 
 # Install Apache and PHP
 sudo apt-get update
+sudo apt-get install iperf3
 sudo apt-get install apache2 -y
 sudo apt-get install php libapache2-mod-php php-mcrypt php-mysql -y
 sudo systemctl restart apache2
@@ -15,15 +16,6 @@ sudo wget https://raw.githubusercontent.com/erjosito/AzureBlackMagic/master/appl
 sudo wget https://raw.githubusercontent.com/erjosito/AzureBlackMagic/master/favicon.ico -P /var/www/html/
 
 
- #assign static IP and change default gw to LISP Router for Intra-Subnet communication across sites:
 
-#cd /etc/network/interfaces.d
-#sudo nano 50-cloud-init.cfg
-
-#auto eth0
-#iface eth0 inet static
-#        address 10.100.100.101
-#        netmask 255.255.255.255
-#        gateway 10.100.100.4
 
 
